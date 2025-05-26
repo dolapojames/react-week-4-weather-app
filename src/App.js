42e0ab4eba984b3daaac00d075b033ffbac2fbb9
dolapojames <oludolapo@hotmail.com>
2025-05-26 21:26:34 +0100

    integrated axios and API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ function App() {
   return (
     <div className="container">
       <div className="App">
-        <Weather />
+        <Weather defaultCity="Lagos" />
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/dolapojames"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dolapo James
+          </a>
+          is
+          <a
+            href="https://github.com/dolapojames/react-week-4-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          and
+          <a
+            href="https://weather-in-react-vscode-240525.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,100 +1,92 @@
 import "./Weather.css";
+import axios from "axios";
+import { useState } from "react";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Lagos",
-    date: "Tues 10:00",
-    humidity: "12",
-    temperature: "12",
-    wind: "12",
-    description: "cloudy",
-  };
-  return (
-    <div className="Weather">
-      <div className="app-container">
-        <form className="search-form">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                required
-                className="search-form-input"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              {" "}
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100 mt-2"
-              />
-            </div>
-          </div>
-        </form>
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherdata] = useState({});
+  function handleResponse(response) {
+    setWeatherdata({
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind,
+      description: response.data.condition.description,
+      date: response.data.time,
+      iconUrl: response.data.icon_url,
+    });
+    setReady(true);
+  }
 
-        <div className="container mt-3">
-          <div className="row justify-content-between">
-            <div className="col">
-              <h1 className="mb-2">{weatherData.city}</h1>
-              <p className=" weather-app-details">
-                <span>{weatherData.date}</span>,{" "}
-                <span>{weatherData.description}</span>
-                <br />
-                Humidity:
-                <span className="weather-strong">{weatherData.humidity}%</span>,
-                Wind:
-                <span className="weather-strong">{weatherData.wind}km/h</span>
-              </p>
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="app-container">
+          <form className="search-form">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city"
+                  required
+                  className="search-form-input"
+                  autoFocus="on"
+                />
+              </div>
+              <div className="col-3">
+                {" "}
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100 mt-2"
+                />
+              </div>
             </div>
-            <div className="col ">
-              <div className="temperature">
-                <div>
-                  <img
-                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
-                    alt=""
-                    className="temperature-icon"
-                  />
-                </div>
-                <div className="temperature-all">
-                  <div className="temperature-value">
-                    {weatherData.temperature}
+          </form>
+
+          <div className="container mt-3">
+            <div className="row justify-content-between">
+              <div className="col">
+                <h1 className="mb-2">{weatherData.city}</h1>
+                <p className=" weather-app-details">
+                  <span>{weatherData.date}</span>,{" "}
+                  <span>{weatherData.description}</span>
+                  <br />
+                  Humidity:
+                  <span className="weather-strong">
+                    {weatherData.humidity}%
+                  </span>
+                  , Wind:
+                  <span className="weather-strong">{weatherData.wind}km/h</span>
+                </p>
+              </div>
+              <div className="col ">
+                <div className="temperature">
+                  <div>
+                    <img
+                      src={weatherData.iconUrl}
+                      alt={weatherData.description}
+                      className="temperature-icon"
+                    />
                   </div>
-                  <div className="temperature-unit">°C</div>
+                  <div className="temperature-all">
+                    <div className="temperature-value">
+                      {weatherData.temperature}
+                    </div>
+                    <div className="temperature-unit">°C</div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        <footer>
-          This project was coded by
-          <a
-            href="https://github.com/dolapojames"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Dolapo James
-          </a>
-          is
-          <a
-            href="https://github.com/dolapojames/react-week-4-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-          and
-          <a
-            href="https://weather-in-react-vscode-240525.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "5ca9a4e04df3dddde0tdc3bec6cd3f5o";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+  }
 }
