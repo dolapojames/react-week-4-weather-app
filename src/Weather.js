3967c5b1a174345ba99ac68bae344ec4ceb4222e
dolapojames <oludolapo@hotmail.com>
2025-05-28 11:20:59 +0100

    included weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import "./Weather.css";
 import { useState } from "react";
-import FormattedDate from "./FormattedDate";
+
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     console.log(response.data.temperature.current);
@@ -16,17 +18,32 @@ export default function Weather(props) {
       city: response.data.city,
       description: response.data.condition.description,
       date: new Date(response.data.time * 1000),
-      iconUrl: response.data.icon_url,
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
     });
 
     setReady(true);
+  }
+
+  function search() {
+    const apiKey = "5ca9a4e04df3dddde0tdc3bec6cd3f5o";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
   }
 
   if (ready) {
     return (
       <div className="Weather">
         <div className="app-container">
-          <form className="search-form">
+          <form className="search-form" onSubmit={handleSubmit}>
             <div className="row">
               <div className="col-9">
                 <input
@@ -35,6 +52,7 @@ export default function Weather(props) {
                   required
                   className="search-form-input"
                   autoFocus="on"
+                  onChange={handleCityChange}
                 />
               </div>
               <div className="col-3">
@@ -47,54 +65,12 @@ export default function Weather(props) {
               </div>
             </div>
           </form>
-
-          <div className="container mt-3">
-            <div className="row justify-content-between">
-              <div className="col">
-                <h1 className="mb-2">{weatherData.city}</h1>
-                <p className=" weather-app-details">
-                  <span>
-                    <FormattedDate date={weatherData.date} />
-                  </span>
-                  ,
-                  <span className="text-capitalize">
-                    {weatherData.description}
-                  </span>
-                  <br />
-                  Humidity:
-                  <span className="weather-strong">
-                    {weatherData.humidity}%
-                  </span>
-                  , Wind:
-                  <span className="weather-strong">{weatherData.wind}km/h</span>
-                </p>
-              </div>
-              <div className="col ">
-                <div className="temperature">
-                  <div>
-                    <img
-                      src={weatherData.iconUrl}
-                      alt={weatherData.description}
-                      className="temperature-icon"
-                    />
-                  </div>
-                  <div className="temperature-all">
-                    <div className="temperature-value">
-                      {Math.round(weatherData.temperature)}
-                    </div>
-                    <div className="temperature-unit">°C</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <WeatherInfo data={weatherData} />
         </div>
       </div>
     );
   } else {
-    const apiKey = "5ca9a4e04df3dddde0tdc3bec6cd3f5o";
-    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,41 @@
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="container mt-3">
+      <div className="row justify-content-between">
+        <div className="col">
+          <h1 className="mb-2">{props.data.city}</h1>
+          <p className=" weather-app-details">
+            <span>
+              <FormattedDate date={props.data.date} />
+            </span>
+            ,<span className="text-capitalize">{props.data.description}</span>
+            <br />
+            Humidity:
+            <span className="weather-strong">{props.data.humidity}%</span>,
+            Wind:
+            <span className="weather-strong">{props.data.wind}km/h</span>
+          </p>
+        </div>
+        <div className="col ">
+          <div className="temperature">
+            <div>
+              <img
+                src={props.data.iconUrl}
+                alt={props.data.description}
+                className="temperature-icon"
+              />
+            </div>
+            <div className="temperature-all">
+              <div className="temperature-value">
+                {Math.round(props.data.temperature)}
+              </div>
+              <div className="temperature-unit">°C</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
